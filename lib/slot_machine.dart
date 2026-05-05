da207cba975fdc16381daf73ce73e71138f0ec48
s0m1k111 <spavel1207@gmail.com>
2026-05-05 12:32:16 +0300

    Update SoundService to use audioplayers for Web and Android

--- lib/slot_machine.dart
+++ lib/slot_machine.dart
@@ -26,7 +26,7 @@ class _SlotMachineState
   var _message = '';
   var _isSpinning = false;
   var _isMuted = false;
-  var _backgroundStarted = false;
+  //var _backgroundStarted = false;
 
   Future<String> _spinReel({
     required int totalTicks,
@@ -54,15 +54,16 @@ class _SlotMachineState
 
   Future<void> _spin() async {
     if (_coins <= 0 || _isSpinning) return;
+    await SoundService.playClick();
     SoundService.playClick();
     setState(() {
       _isSpinning = true;
       _message = '';
     });
-    if (!_backgroundStarted) {
-      SoundService.playBackground();
-      _backgroundStarted = true;
-    }
+    // if (!_backgroundStarted) {
+    //   SoundService.playBackground();
+    //   _backgroundStarted = true;
+    // }
     final result1 = await _spinReel(
       totalTicks: 10,
       onTick: (val) =>
@@ -81,26 +82,29 @@ class _SlotMachineState
     await Future.delayed(
       Duration(milliseconds: 300),
     );
-    setState(() {
-      _isSpinning = false;
-      if (result1 == result2 &&
-          result2 == result3) {
-        if (result1 ==
-            'assets/images/seven.png') {
-          _coins += 10;
-          _message = 'ДЖЕКПОТ! +10 монет';
-          SoundService.playJackpot();
-        } else {
-          _coins += 3;
-          _message = 'Победа!  +3 монеты';
-          SoundService.playWin();
-        }
+    String newMessage;
+    int coinsChange;
+    if (result1 == result2 &&
+        result2 == result3) {
+      if (result1 == 'assets/images/seven.png') {
+        coinsChange = 10;
+        newMessage = "ДЖЕКПОТ! +10 монет";
+        await SoundService.playJackpot();
       } else {
-        _coins -= 1;
-        _message = 'Попробуй ещё раз -1 монета';
-        SoundService.playLose();
+        coinsChange = 3;
+        newMessage = 'Победа! +3 монеты';
+        await SoundService.playWin();
       }
-    });
+    } else {
+      coinsChange = -1;
+      newMessage = 'Попробуй ещё раз -1 монета';
+      await SoundService.playLose();
+      setState(() {
+        _isSpinning = false;
+        _coins += coinsChange;
+        _message = newMessage;
+      });
+    }
   }
 
   void _reset() {
